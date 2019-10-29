8c97b5c8ce203d36c0e797216e1173c9a48ebd78
Anthony Whyte <anthwhyte@gmail.com>
2019-10-29 10:56:56 -0400

    Add alternate problems.

---
+++ code/gradescope/midterm/midterm_alt1_solution.py
@@ -0,0 +1,52 @@
+# SI 506 Midterm Exam - Practical Question 1
+
+# Fill in the missing places in the code below to write a function named <filter_and_sort_strings>
+# that takes two arguments. The first argument is a list of strings called <str_list>, and the
+# second argument is an integer called <maximum_len>. Give the <maximum_len> argument a default
+# value of 5. The function should loop through <str_list>, append all the elements with *less
+# than or equal to* <maximum length> to a new list called <filtered>. Then, the code should return
+# <filtered>, but sorted alphabetically.
+
+# The final few lines of this code are provided by us to test your code. If done correctly,
+# after this code is executed, the following should be the value of <result> and
+# printed out to the terminal:
+
+# ['Idaho', 'Iowa', 'Maine', 'Texas']
+
+# The following is the framework of the code you should write...fill in the places where
+# prompted below.
+
+# # # # START CODING HERE # # # # # # # # # # # # # # # # # # # # #
+
+# Fill in the correct arguments to the below function definition. Don't
+# forget the default argument!
+def filter_and_sort_strings(str_list,maximum_len=5): # <-- Write the proper arguments here.
+    filtered = []
+
+    # Use a "for" loop to loop through each element of <str_list>.
+    # You will need to come up with a variable that contains the looped values
+    # from <str_list>.
+    for item in str_list: # <-- Write your "for" statement here.
+
+        # Now, use an "if" conditional to check if each element of <str_list> has *less than or
+        # equal to* the number of characters specified by maximum_len.
+        if len(item) <= maximum_len: # <-- Write your "if" statement here.
+
+            # Now, append any elements of <str_list> that meet the above criteria (the
+            # "if" statement) to the list <filtered>
+            filtered.append(item) # <-- Write your code here.
+
+    # Finally, use "return" and "sorted" to return a sorted list of the elements that
+    # met the criteria from your "if" statement.
+    return sorted(filtered) # <-- Write your "return" statement here. Don't forget to sort the <filtered> list!
+
+# # # # STOP CODING HERE # # # # # # # # # # # # # # # # # # # # #
+# Don't change anything below this line!
+
+# Use the below lines test your function. DO NOT CHANGE ANYTHING BELOW THIS LINE.
+
+states = ['Michigan','Indiana','Iowa','Washington','Texas',
+          'Florida','Wyoming','Maine','Idaho','Nevada']
+
+result = filter_and_sort_strings(states)
+print(f"\nResultant list: {result}\n")
---
+++ code/gradescope/midterm/midterm_alt2_solution.py
@@ -0,0 +1,70 @@
+# SI 506 Midterm Exam - Practical Question 2
+
+# Fill in the missing places in the code below to write a function named <write_planets> that
+# takes two (2) arguments, a list of strings called <contents> and a string called <filename>.
+# The <contents> list will be a list of celestial objects in the following format:
+
+# ["name-type","name-type",...etc]
+
+# The function should loop through <contents>, and for each element that has a type of 'planet', write its
+# name to the file specified by <filename>.
+
+# The final several lines of this file are provided by us for you to test your code.
+# If done correctly, after this code is executed, the contents of "midterm.txt" should contain
+# three lines of text, in the following order:
+
+"""Mercury
+Venus
+Earth
+Mars
+"""
+
+# The following is the framework of the code you should write...fill in the places where
+# prompted below.
+
+def write_planets(contents,filename):
+
+# # # # START CODING HERE # # # # # # # # # # # # # # # # # # # # #
+
+    # Use a "with" statement in the below line to open the file that you will write to.
+    # Use the argument variable name, not the actual file name in your open() statement.
+    # You will need to come up with a file object variable.
+    with open(filename,'w') as f: # <-- Write the "with" statement here.
+
+        # Now initiate a for-loop over <contents>.
+        # You will need to come up with a variable that contains the looped values from <contents>.
+        for item in contents: # <-- Write the "for" statement here.
+
+            # Now, split each element in <contents> on the hyphen ("-") so that you
+            # have a list containing two strings, the name and type of the celestial object.
+            info = item.split('-') # <-- Write your "split" statement here.
+
+            # Now, check if the the current element of the list is type 'planet' or not by using
+            # an "if" conditional. You will need to use list slicing here.
+            if info[1] == 'planet': # <-- Write your "if" statement here.
+
+                # Finally, write the name of each element that passed the above conditional to
+                # the file specified by <filename>. Don't forget to add a newline character
+                # ("\n") to the end of each string that you write!
+                f.write(info[0]+'\n') # <-- Write the "write" statement here.
+
+# # # # STOP CODING HERE # # # # # # # # # # # # # # # # # # # # #
+# Don't change anything below this line!
+
+    return None # There are no returned variables for this function.
+
+# Feel free to add a print statement within <write_long_strings> to check if what you are writing
+# to <filename> is correct. You may also want to open the file you created to check.
+
+# Use the below lines test your function. DO NOT CHANGE ANYTHING BELOW THIS LINE.
+
+celestial_objects = ["Titan-satellite",
+                     "Mercury-planet",
+                     "Halley's Comet-comet",
+                     "Venus-planet",
+                     "Pluto-protoplanet",
+                     "Earth-planet",
+                     "Ganymede-satellite",
+                     "Mars-planet"]
+
+write_planets(celestial_objects,"midterm.txt")
